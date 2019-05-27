a22235f00afee4297780197b65b25f5e48150a87
ryuhei.sugita <ryuhei.sugita@rakus.co.jp>
2019-05-27 12:06:28 +0900

    プロジェクト新規に作成

---
+++ src/main/java/com/example/GitSampleSugitaApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitSampleSugitaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitSampleSugitaApplication.class, args);
+	}
+
+}
